81dfbb33e9d4eeb8c4b67db2100a329dd58ec1da
pgm-yusufkilli <yusuf.killi@student.arteveldehs.be>
2025-02-18 09:34:06 +0100

    ejs has been added

--- src/app.js
+++ src/app.js
@@ -5,13 +5,19 @@ import path from "path";
 // create an instance of express
 const app = express();
 
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
+
 // serve static files from the public folder
 // they can be accessed from the root of the site (e.g. /assets/images/dino_07.png) 🦕
 app.use(express.static("public"));
 
 // GET route to serve the index.html file
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve("src", "views", "index.html"));
+  res.render("home", {
+    title: "Dinasaurs are awesome!",
+    content: "This is the content of the home page.",
+  });
 });
 
 // start the server, listen on port defined in .env file
